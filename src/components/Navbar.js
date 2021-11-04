948a6b4fcef567f64a5e4251632c31129b3e10c0
Abel Lavieri <abel.lavieri@gmail.com>
2021-11-04 09:25:17 -0400

    Add Navbar Component and pages

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Books',
+    },
+    {
+      id: 2,
+      path: '/categories',
+      text: 'Categories',
+    },
+  ];
+
+  return (
+    <nav className="nav-bar">
+      <div className="navbar-container">
+        <span className="brand-name">BookStore</span>
+        <ul className="nav-links">
+          {links.map((link) => (
+            <li key={link.id}>
+              <NavLink to={link.path} activeClassName="active-link" exact>
+                {link.text}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
---
+++ src/pages/Books.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Books = () => (
+  <div>
+    <h1>Books Under construction</h1>
+  </div>
+);
+
+export default Books;
