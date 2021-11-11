c618292a723b9bd67b248fcbf3d3cbdb0eecf43d
Abel Lavieri <abel.lavieri@gmail.com>
2021-11-10 20:51:28 -0400

    Add method to remove books from the list

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,32 +1,38 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 
 const Book = ({
-  category, title, author, currentChapter,
-}) => (
-  <div className="book-container">
-    <div className="book-info">
-      <h3 className="book-category">{category}</h3>
-      <h2 className="book-title">
-        {' '}
-        {title}
-      </h2>
-      <h4 className="book-author">{author}</h4>
-      <div className="options">
-        <button type="button">Comments</button>
-        <button type="button">Remove</button>
-        <button type="button">Edit</button>
+  id, category, title, author, currentChapter,
+}) => {
+  const dispatch = useDispatch();
+  const handleClick = () => dispatch(removeBook(id));
+  return (
+    <div className="book-container">
+      <div className="book-info">
+        <h3 className="book-category">{category}</h3>
+        <h2 className="book-title">
+          {title}
+        </h2>
+        <h4 className="book-author">{author}</h4>
+        <div className="options">
+          <button type="button">Comments</button>
+          <button type="button" onClick={handleClick}>Remove</button>
+          <button type="button">Edit</button>
+        </div>
+      </div>
+      <div className="book-progress">
+        <div className="progress" />
+        <div className="current-chapter">Current Chapter</div>
+        <div className="book-chapter">{currentChapter}</div>
+        <button type="button">Update progress</button>
       </div>
     </div>
-    <div className="book-progress">
-      <div className="progress" />
-      <div className="current-chapter">Current Chapter</div>
-      <div className="book-chapter">{currentChapter}</div>
-      <button type="button">Update progress</button>
-    </div>
-  </div>
-);
+  );
+};
 
 Book.propTypes = {
+  id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
   category: PropTypes.string.isRequired,
