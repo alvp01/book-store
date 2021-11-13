c4d83f306e167748f58bc2d901b874fbda4e1b3d
Abel Lavieri <abel.lavieri@gmail.com>
2021-11-13 18:18:57 -0400

    Add style to form component

--- src/index.js
+++ src/index.js
@@ -10,13 +10,15 @@ import { Provider } from 'react-redux';
 import Books from './pages/Books';
 import Categories from './pages/Categories';
 import store from './redux/configureStore';
+import Navbar from './components/Navbar';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
-        <div className="container d-flex justify-center align-center">
+        <div className="container d-flex justify-center align-center border-box">
+          <Navbar />
           <Switch>
             <Route path="/categories">
               <Categories />
--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import BookList from '../components/BookList';
 import BookForm from '../components/BookForm';
-import Navbar from '../components/Navbar';
 
 const Books = () => (
-  <div className="book-page-container d-flex flex-column">
-    <Navbar />
+  <div className="book-page-container d-flex flex-column border-box w-100">
     <BookList />
     <BookForm />
   </div>
